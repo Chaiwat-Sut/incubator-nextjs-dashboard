fcf5bc8ce724353f6149abecfa763bdb62616ace
Chaiwat Suttiprapha <a667459@Chaiwat-2.local>
2024-01-12 13:14:23 +0700

    add dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import AcmeLogo from "./ui/acme-logo";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        {<AcmeLogo />}
       </div>
 
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -24,8 +26,24 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
+
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* Insert image here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="this is desktop image"
+          />
+
+          <Image
+            src="/hero-mobile.png"
+            width={600}
+            height={420}
+            className="md:hidden block"
+            alt="this is desktop image"
+          />
         </div>
       </div>
     </main>
